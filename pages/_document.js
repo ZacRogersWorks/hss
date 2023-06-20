45853af2f26b905448592fce38f3c2e137b07393
Zac Rogers <hello@zacrogers.works>
2023-06-20 16:29:17 -0700

    Testing typekit font issue

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="stylesheet" href="https://use.typekit.net/dop2uqb.css" />
+      {/* <link rel="stylesheet" href="https://use.typekit.net/dop2uqb.css" /> */}
       </Head>
       <body>
         <Main />
