768f1233757b9cf1d9542778dd57877e2b343eb0
Zac Rogers <hello@zacrogers.works>
2023-06-20 17:15:47 -0700

    Adding bootstrap

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,10 +8,12 @@ export default function Document() {
       {/* <link rel="stylesheet" href="https://use.typekit.net/dop2uqb.css" /> */}
       <link rel="stylesheet" href="/styles/globals.css"/>
       <link rel="stylesheet" href="/styles/Home.css"/>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></link>
       </Head>
       <body>
         <Main />
         <NextScript />
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></Script>
       </body>
     </Html>
   )
